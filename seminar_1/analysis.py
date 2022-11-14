d4052ec60b7b554e655f7217242bf2ac1cab6a39
al-pi314 <aleksander.piciga@gmail.com>
2022-11-14 18:54:09 +0100

    implemented pooling and csv saving

--- seminar_1/analysis.py
+++ seminar_1/analysis.py
@@ -1,80 +1,70 @@
 import itertools
 import os.path
 from multiprocessing import Pool
-from random import seed
-from threading import Lock
-from time import time as seconds
+from random import seed, random
 
 import pandas as pd
 from maze_ga import MazeGa
 from numpy import linspace
 
-generations_scores_dataframe_lock = Lock()
-runs_dataframe_lock = Lock()
-generations_scores_filewrite_lock = Lock()
-runs_filewrite_lock = Lock()
-global_write_lock = Lock()
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 def encode_path(path):
     # convert path array into a string that can be later used for creating an agent
     return ''.join([str(x) for x in path])
 
+def error_callback(e):
+    print("ERROR: ", e)
+
 def save_to_csv():
+    print("saving to csv")
+
     # save runs data to csv
-    runs_filewrite_lock.acquire()
-    runs.to_csv(save_dir + "runs.csv", index=True)
-    runs_filewrite_lock.release()
+    runs.to_csv(analysis_runs_file, index=True)
 
     # save generations_scores to csv
-    generations_scores_filewrite_lock.acquire()
-    generations_scores.to_csv(save_dir + "generations_scores.csv", index=True)
-    generations_scores_filewrite_lock.release()
+    generations_scores.to_csv(analysis_generations_scores_file, index=True)
 
-def save_to_df(solutions_fintness, best_solutions, input, run_id):
-    print("save_to_df", run_id)
+def save_to_df(solutions_fintness, best_solutions, parameters, run_id):
+    global runs, generations_scores
     # add each solution fitness in order to generation_scores data frame
-    generations_scores_dataframe_lock.acquire()
+    generation_stats = []
     for i in range(len(solutions_fintness)):
-        generations_scores.loc[-1] = [run_id, i, solutions_fintness[i], encode_path(best_solutions[i])]
-        generations_scores.index = generations_scores.index + 1
-    generations_scores_dataframe_lock.release()
-
+        generation_stats.append([run_id, i, solutions_fintness[i], encode_path(best_solutions[i])])
+    generations_scores = generations_scores.append(pd.DataFrame(generation_stats, columns=generations_scores_columns), ignore_index=True)
 
     # add run data to runs data frame + add run_id that can be mapped to generations_scores data frame
-    runs_dataframe_lock.acquire()
-    runs.loc[-1] = [input[key] for key in runs.columns]
-    runs.index = runs.index + 1
-    runs_dataframe_lock.release()
+    parameters["run_id"] = run_id
+    runs = runs.append(parameters, ignore_index=True)
 
-    # check writes
-    print(run_id, save_on_n_runs)
-    if run_id % save_on_n_runs == 0:
-        print("time needed per task", (round(seconds() * 1000) - start_time) / run_id)
-        save_to_csv()
+def save_results(results):
+    # save results to data frame
+    for solutions_fintness, best_solutions, parameters, run_id in results:
+        save_to_df(solutions_fintness, best_solutions, parameters, run_id)
+    save_to_csv()
 
-def execute_combination(raw):
-    file_data, enumerated_input = raw
-    run_id, input = enumerated_input
-
+def execute_combination(run_id, parameters, maze_strings):
     # extract maze file path and load it
-    maze_string = file_data[input["maze_file"]]
-    maze_ga = MazeGa(maze_string, use_custom_functions=input["use_custom_functions"], valid_only=input["valid_only"], show_each_n=0, threads=5)
+    maze_string = maze_strings[parameters["maze_file"]]
+    maze_ga = MazeGa(maze_string, use_custom_functions=parameters["use_custom_functions"], valid_only=parameters["valid_only"], show_each_n=0, threads=5)
 
     # execute ga algorithem and retrive results
-    solutions_fintness, best_solutions = maze_ga.run(input["generations"], input["population_size"], input["parents"], input["mutation_probability"], input["elitism"])
+    solutions_fintness, best_solutions = maze_ga.run(parameters["generations"], parameters["population_size"], parameters["parents"], parameters["mutation_probability"], parameters["elitism"])
 
     # at the end write all data to data frame (uses locking)
     print(run_id, "finished")
-    
-    save_to_df(solutions_fintness, best_solutions, input, run_id) 
+    return solutions_fintness, best_solutions, parameters, run_id
 
 if __name__ == "__main__":
     # set random seed
     seed(100)
 
-    # directories paths (must exist)
-    directory = "./mazes/"
-    save_dir = "./analysis/"
+    # files
+    analysis_runs_file = "./analysis/runs.csv"
+    analysis_generations_scores_file = "./analysis/generations_scores.csv"
+    maze_directory = "./mazes/"
 
     # all possible parameters
     parameters = {
@@ -87,48 +77,62 @@ if __name__ == "__main__":
         "elitism": [0, 0.01, 0.1],
         "use_custom_functions": [True, False],
     }
+    runs_columns = ["maze_file", "generations", "valid_only", "population_size", "parents", "mutation_probability", "elitism", "use_custom_functions", "run_id"]
+    generations_scores_columns = ["run_id", "generation", "fitness", "path"]
 
-    file_data = {}
-    for name in parameters["maze_file"]:
-        f = open(directory + name, "r")
-        file_data[name] = f.read()
-        f.close()
+    runs = pd.DataFrame(columns=runs_columns)
+    generations_scores = pd.DataFrame(columns=generations_scores_columns)
+
+    if os.path.isfile(analysis_runs_file):
+        runs = pd.read_csv(analysis_runs_file, index_col=0)
+        if set(runs_columns) != set(runs.columns):
+            print("ERROR: runs file columns missmatch")
+            print("expected: ", runs_columns)
+            print("got: ", runs.columns)
+            exit(1)
+    
+    if os.path.isfile(analysis_generations_scores_file):
+        generations_scores = pd.read_csv(analysis_generations_scores_file, index_col=0)
+        if set(generations_scores_columns) != set(generations_scores.columns):
+            print("ERROR: generations_scores file columns missmatch")
+            print("expected: ", generations_scores_columns)
+            print("got: ", generations_scores.columns)
+            exit(1)
+
+    # read maze strings
+    maze_strings = {}
+    for maze_file in parameters["maze_file"]:
+        with open(os.path.join(maze_directory, maze_file), "r") as file:
+            maze_strings[maze_file] = file.read()
+
 
     # every possible combination of parameters
     values_combinations = list(itertools.product(*parameters.values()))
     combinations = [
         dict(zip(parameters.keys(), combination)) for combination in values_combinations
     ]
+
     combinations_len = len(combinations)
     print(combinations[0])
     print("combinations size", combinations_len)
 
-    # data frames that store all run results
-    runs = pd.DataFrame(columns=list(parameters.keys()) + ["run"])
-    generations_scores = pd.DataFrame(columns=["run", "generation", "score", "path"])
-
-    # read existing files
-    if os.path.isfile(save_dir + "runs.csv"):
-        runs = pd.read_csv(save_dir + "runs.csv", index_col=0)
-        print("runs loaded from csv")
-    if os.path.isfile(save_dir + "generations_scores.csv"):
-        generations_scores = pd.read_csv(save_dir + "generations_scores.csv", index_col=0)
-        print("generations_scores loaded from csv")
-
-    # start time
-    start_time = round(seconds() * 1000)
-
     # save on n runs
-    save_on_n_runs = 100
+    save_on_n_runs = 10
 
     # thread pool parameters
-    max_workers = 10
+    max_processes = 10
 
     # start thread pool
-    with Pool(processes=max_workers) as executor:
-        for future in executor.map(execute_combination, [(file_data, v) for v in enumerate(combinations)]):
-            future.results()
-        
+    with Pool(processes=max_processes) as pool:
+        solved = set(runs["run_id"].unique())
+        arguments = [(i, combinations[i], maze_strings) for i in range(len(combinations)) if i not in solved]
+        print("unsolved", len(arguments))
+        for i in range(0, len(arguments), save_on_n_runs):
+            sub_arguments = arguments[i:i + save_on_n_runs]
+            results = pool.starmap(execute_combination, sub_arguments)
+            save_results(results)
+        pool.close()
+        pool.join()
 
     # final save
     save_to_csv()
